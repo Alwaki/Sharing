96109f87a59ca52e54a53306f4bb38ff1c2d5908
Alexander <59147412+Alwaki@users.noreply.github.com>
2021-02-24 14:53:31 +0100

    Update shape_detector.py

--- shape_detector.py
+++ shape_detector.py
@@ -41,10 +41,10 @@ values, which can be used if no changes are necessary.
 image_topic     = "raspicam_node/image"
 detection_topic = "tag_detections_image"
 image_encoding  = "bgr8"
-area_min        = 20                # minimum area of contour for detection
+area_min        = 200                # minimum area of contour for detection
 area_max        = 8000              # maximum area of contour for detection
-thresh_1        = 25
-thresh_2        = 25
+thresh_1        = 100
+thresh_2        = 100
 cnt_ret         = 'RETR_EXTERNAL'
 cnt_met         = 'CHAIN_APPROX_NONE'
 font            = 'FONT_HERSHEY_COMPLEX'
@@ -62,29 +62,46 @@ This section should not be changed by users.
 import cv2
 import rospkg
 import rospy
+import numpy as np
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-
 
 ##########################################################################
 ##                          FUNCTION DEFINITIONS
 ##########################################################################
 '''
-This section contains function definitions. Descriptions of these are also
+This section contains definitions. Descriptions of these are also
 included, and a brief mention of important used variables (input & output)
 '''
 
-'''
-Function Name:          getContours()
-
-Description:
-
-Used Variables:         - image:
-                        - contoured_image:
-
-'''
 def getContours(image, output_image):
+
+    '''
+
+    Function Name:          getContours()
+
+    Description:            This function uses an image which has recieved previous
+                            treatment in the form of blur, grayscale, canny, and
+                            dilation. It uses the findContours function from cv,
+                            and detects contours in the image. These contours are
+                            passed through a for loop, in which noise is removed
+                            by verifying a legal size of the encompassed contour
+                            area. By then connecting the contour points, we can see
+                            if there are a number of lines which match a given
+                            geometric shape. If there is a legal shape, this
+                            information is written to the original image (without
+                            blur, grayscale, etc) and given as output.
+
+    Used Variables:         - image:        input image (modified to ease
+                                            contour search)
+
+                            - output_image: Original image which is returned
+                                            with added information such as
+                                            type, size, detection box.
+
+    '''
+
     contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     for cnt in contours:
@@ -97,62 +114,115 @@ def getContours(image, output_image):
             lines = len(approx)
             type = ""
 
+            flag = False
+
             if(lines==3):
                 type = "triangle"
-                print("triangle!")
+                flag = True
             elif(lines==4):
+                flag = True
                 type = "rectangle"
-                print("recntangle!")
             elif(lines==5):
                 type = "pentagon"
-                print("pentagon!")
-            else:
-                type = "unidentified"
-                print("nothing!")
+                flag = True
 
-            x, y, w, h = cv2.boundingRect(approx)
-            cv2.rectangle(output_image, (x, y), (x+w, y+h), (0,255, 0), 5)
-            cv2.putText(output_image, "Points: " + str(len(approx)), (x+w+20, y+20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
-            cv2.putText(output_image, "Area: " + str(int(area)), (x+w+20, y+45), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
-            cv2.putText(output_image, "Type:" + type, (x+w+20, y+70), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
+            if flag:
+                x, y, w, h = cv2.boundingRect(approx)
+                cv2.rectangle(output_image, (x, y), (x+w, y+h), (0,0, 255), 5)
+                cv2.putText(output_image, "Points: " + str(len(approx)), (x+w+20, y+20), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
+                cv2.putText(output_image, "Area: " + str(int(area)), (x+w+20, y+45), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
+                cv2.putText(output_image, "Type:" + type, (x+w+20, y+70), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,255,0), 2)
+
     return output_image
 
-def image_manipulate(image):
-        output_image =  image.copy()
-        blurred_image = cv2.GaussianBlur(image, (7, 7), 1)
-        gray_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2GRAY)
-        canny_image = cv2.Canny(gray_image, thresh_1, thresh_2)
-        kernel = np.ones((5,5))
-        dilated_image = cv2.dilate(canny_image, kernel, iterations = 1)
-        output_image = Contours(dilated_image, output_image)
-        return output_image
-'''
-class
-'''
+
+def imageManipulate(image):
+
+    '''
+
+    Function Name:          imageManipulate()
+
+    Description:            This function takes an image, stores a copy of it
+                            and passes the image several cv methods which makes
+                            it easier to find contours in the image.
+                            The contours are found with the help of getContours().
+
+    Used Variables:         - image:        input image with cv format.
+
+                            - output_image: returned value, original image with
+                                            added information regarding shape
+                                            detection.
+
+
+    '''
+
+    output_image =  image.copy()
+    blurred_image = cv2.GaussianBlur(image, (7, 7), 1)
+    gray_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2GRAY)
+    canny_image = cv2.Canny(gray_image, thresh_1, thresh_2)
+    kernel = np.ones((5,5))
+    dilated_image = cv2.dilate(canny_image, kernel, iterations = 1)
+    output_image = getContours(dilated_image, output_image)
+    return output_image
 
 class detector:
+
+    '''
+    Class Name:             imageManipulate()
+
+    Description:            This function takes an image, stores a copy of it
+                            and passes the image several cv methods which makes
+                            it easier to find contours in the image.
+                            The contours are found with the help of getContours().
+
+    Attributes:             - bridge:
+
+                            - image_sub:
+
+                            - image_pub:
+
+    Methods:                - callback():
+
+
+    '''
+
     def __init__(self):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(image_topic, Image, self.callback, queue_size=2)
         self.image_pub = rospy.Publisher(detection_topic, Image, queue_size=2)
 
     def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, image_encoding)
-            output_image = image_manipulate(cv_image)
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(output_image, image_encoding))
-        except:
-            print("Error callback")
+        cv_image = self.bridge.imgmsg_to_cv2(data, image_encoding)
+        output_image = imageManipulate(cv_image)
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(output_image, image_encoding))
+
 
 def main():
-    print("HIWAPFOAWIOHALIWHFAWGF")
-    image_converter = detector()
+
+    '''
+
+    Function Name:          main()
+
+    Description:            The main function of the program. Initializes the
+                            node as per ROS default, creates the detection
+                            instance, and hands over control to roscore.
+
+    Used Variables:         n/a
+
+    '''
+
     rospy.init_node("shape_detector.py", anonymous = True)
-    print("HIWAPFOWUBVADUIYGVWDUYAWGV")
+    image_converter = detector()
     rospy.spin()
 
 ##########################################################################
 ##                          DRIVER CODE
 ##########################################################################
+
+'''
+This section is intended to initialize any code which requires startup prompt
+to be used in applications.
+'''
+
 if __name__ == '__main__':
     main()
